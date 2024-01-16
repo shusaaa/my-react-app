4d44e042d02443d8e253b4dbbeadeb5b88b9e1ad
zhaoxin <1473312746@qq.com>
2024-01-16 14:38:32 +0800

    update

---
+++ src/view/index.tsx
@@ -0,0 +1,13 @@
+export default function HomePage (){
+    const name = '我'
+    return (
+        <>
+            <h1>{name}</h1>
+            <h1>{name}</h1>
+            <h1>{name}</h1>
+            <h1>{name}</h1>
+            <h1>{name}</h1>
+        </>
+        
+    )
+}
